e6776ba9d406dd6e6c2030bb68350a79024d3ef2
Jonathan Garcia <jonathan.porley15@gmail.com>
2024-06-26 23:53:00 -0300

    Vaciado

--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-import {connect} from 'mongoose'
-
-    
-export const connectDB = () => {
-        connect('mongodb+srv://coder:coder.2024@cluster0.yyfgeas.mongodb.net/ecommerce')
-        console.log('Connected to DB')
-    }
--- src/utils/jwt.js
+++
@@ -1,4 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-export const PRIVATE_KEY = 'coder-secret'
-export const generateToken = user => jwt.sign({user},PRIVATE_KEY, {expiresIn:'24h'})
